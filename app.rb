946d475023a7140ae7777ea90b5017e5184a13d5
Adriana <am20dipi@alum.siena.edu>
2021-01-22 16:12:37 -0500

    commit tree 15451dbdced60048b066486457b7b62cb0589a00

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
+require_relative 'models/piglatinizer.rb'
 
 class App < Sinatra::Base
+
+    get '/' do 
+        erb :user_input
+    end
+
+    post '/' do 
+        input_from_user = params[:user_input]
+        @piglatinized_input = PigLatinizer.new(input_from_user)
+
+        erb :results
+
+    end
 end
---
+++ models/piglatinizer.rb
@@ -0,0 +1,11 @@
+class PigLatinizer
+    attr_reader :words
+
+    def initialize
+        @words = words
+    end
+
+    def piglatinize
+
+    end
+end
